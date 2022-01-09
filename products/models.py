fbc4ea0c253236f5aedcc476be16ea9557d73f5e
newdana01 <newdana01@gmail.com>
2022-01-10 05:19:19 +0900

    AllergyDrink class 수정(ManyToManyField 사용)

--- products/models.py
+++ products/models.py
@@ -9,14 +9,14 @@ class Menu(models.Model):
         db_table = 'menu'
 
 class Category(models.Model):
-    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     name = models.CharField(max_length=45)
 
     class Meta:
         db_table = 'categories'
 
 class Drink(models.Model):
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     korean_name = models.CharField(max_length=45)
     english_name = models.CharField(max_length=45)
     description = models.TextField()
@@ -25,7 +25,7 @@ class Drink(models.Model):
         db_table = 'drinks'
 
 class Image(models.Model):
-    drink_id = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
     image_url = models.CharField(max_length=2000)
 
     class Meta:
@@ -33,13 +33,14 @@ class Image(models.Model):
 
 class Allergy(models.Model):
     name = models.CharField(max_length=45)
+    allergy_drink = models.ManyToManyField(Drink, through='AllergyDrink')
 
     class Meta:
         db_table = 'allergies'
 
 class AllergyDrink(models.Model):
-    allergy_id = models.ForeignKey(Allergy, on_delete=models.CASCADE)
-    drink_id = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    allergy= models.ForeignKey(Allergy, on_delete=models.CASCADE)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'allergy_drink'
@@ -54,8 +55,8 @@ class Size(models.Model):
 
 
 class Nutrition(models.Model):
-    drinks_id = models.ForeignKey(Drink, on_delete=models.CASCADE)
-    size_id = models.ForeignKey(Size, on_delete=models.CASCADE)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
     one_serving_kcal = models.DecimalField(max_digits=10, decimal_places=2)
     sodium_mg = models.DecimalField(max_digits=10, decimal_places=2)
     saturated_fat_g = models.DecimalField(max_digits=10, decimal_places=2)
